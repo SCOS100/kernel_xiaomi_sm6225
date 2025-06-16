8787a573a2dcba94a93a86c052cdd3882e846033
SCOS100 <hsc100zz@gmail.com>
2025-06-16 15:18:43 +0200

    Revert "aaaaaa"
    This reverts commit 2725f1e9554e5461076d51635b46b057da539611.

--- build.sh
+++ build.sh
@@ -1,87 +1,39 @@
-#!/bin/bash
-#
-# Compile script for uvite Kernel
-# Copyright (C) 2020-2021 Adithya R.
+#!/bin/sh
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 2725f1e9554e (aaaaaa)
 if [ "$1 " = "env " ]; then echo "Type 'exit' to quit environment."; ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 debian_chroot="Redmi Note 11/NFC Kernel" bash; exit; fi
 
 =======
 >>>>>>> parent of e71c15230078 (a)
 RELEASE=$(make -j$(nproc --all) kernelrelease)
-=======
-SECONDS=0 # builtin bash timer
-ZIPNAME="uvite-$(date '+%Y%m%d-%H%M')-spes.zip"
-TC_DIR="$(pwd)/tc/clang-r450784e"
-AK3_DIR="$(pwd)/android/AnyKernel3"
-DEFCONFIG="vendor/spes-perf_defconfig"
->>>>>>> parent of 0e843872385a (Added fixes and other stuff to begin new fork.)
 
-if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-   head=$(git rev-parse --verify HEAD 2>/dev/null); then
-	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+# Build the kernel.
+if [ ! -e .config ]; then
+	echo "Setting defconfig"
+	cp arch/arm64/configs/spesnd_defconfig .config
 fi
-
-export PATH="$TC_DIR/bin:$PATH"
-export KBUILD_BUILD_USER=nobody
-export KBUILD_BUILD_HOST=android-build
-
-if ! [ -d "$TC_DIR" ]; then
-	echo "AOSP clang not found! Cloning to $TC_DIR..."
-	if ! git clone --depth=1 -b 14 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
-		echo "Cloning failed! Aborting..."
-		exit 1
-	fi
+if [ -d AnyKernel3 ]; then rm -r AnyKernel3; fi
+git clone https://github.com/CHRISL7/AnyKernel3 -b master
+cd AnyKernel3
+sed -i "s/kernel.string=$(grep "kernel.string" anykernel.sh | cut -d= -f2)/kernel.string=$RELEASE/g" anykernel.sh
+sed -i "s/kernel.compiler=$(grep "kernel.compiler" anykernel.sh | cut -d= -f2)/kernel.compiler=\"llvm and clang\"/g" anykernel.sh
+if ! grep "=m$" ../.config 2>/dev/null >/dev/null; then
+	sed -i "s/do.modules=$(grep "do.modules" anykernel.sh | cut -d= -f2)/do.modules=0/g" anykernel.sh
 fi
-
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-	cp out/defconfig arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
-	exit
-fi
-
-if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG
-	cp out/.config arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
-	exit
-fi
-
-if [[ $1 = "-c" || $1 = "--clean" ]]; then
-	rm -rf out
-fi
-
-mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
-
-echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 Image.gz dtb.img dtbo.img 2> >(tee log.txt >&2) || exit $?
-
-kernel="out/arch/arm64/boot/Image.gz"
-dtb="out/arch/arm64/boot/dtb.img"
-dtbo="out/arch/arm64/boot/dtbo.img"
-
-if [ -f "$kernel" ]; then
-	echo -e "\nKernel compiled succesfully! Zipping up...\n"
-	if [ -d "$AK3_DIR" ]; then
-		cp -r $AK3_DIR AnyKernel3
-	elif ! git clone -q https://github.com/CHRISL7/AnyKernel3 -b master; then
-		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
-		exit 1
-	fi
-	cp $kernel $dtb $dtbo AnyKernel3
-	rm -rf out/arch/arm64/boot
-	cd AnyKernel3
-	git checkout master &> /dev/null
-	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
-	cd ..
-	rm -rf AnyKernel3
-	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-	echo "Zip: $ZIPNAME"
-else
-	echo -e "\nCompilation failed!"
+rm README*
+cd ..
+make -j$(nproc --all) INSTALL_MOD_PATH=$(pwd)/AnyKernel3/modules/ ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 Image.gz dtbo.img
+if ! [ "$?" -eq "0" ]; then
+	echo "The compilation did not go well. Exiting..."
 	exit 1
+else
+	cp arch/arm64/boot/Image.gz arch/arm64/boot/dtbo.img AnyKernel3
+	cd AnyKernel3
+	zip -r9 ../SPESND.zip * -x .git README.md *placeholder
+	cd ..
+	echo "Zip saved as SPESND.zip. Please flash it into your phone's recovery if supported."
 fi
-
