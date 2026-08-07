3bccfa9fe0bc47b2bfa309e53dfa00537e6c7735
SCOS100 <hsc100zz@gmail.com>
2026-08-07 16:34:19 +0200

    Automation! (Droidian) new file:   .github/workflows/build_and_publish.yml modified:   .gitignore modified:   build_droidian.sh

--- build_droidian.sh
+++ build_droidian.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-if [ "$USER" != "root" ]; then { echo "Please run as root!"; exit 1; }; fi
-if [ -z "$SUDO_USER" ]; then { echo "Please run with sudo (or set SUDO_USER to root if you are running as root)!"; exit 1; }; fi
-
-if ! docker -v >/dev/null; then
-	echo "Please install docker!!!"
-	exit 1
-fi
-
 echo "CREATE dirs"
 mkdir -p debian/source
 cp arch/arm64/configs/spes_kernel_info.mk debian/kernel-info.mk
@@ -33,6 +25,3 @@ docker run --dns 1.1.1.1 --rm \
 	bash -c "apt install linux-packaging-snippets; cd buildd/sources; rm -f debian/control; debian/rules debian/control; RELENG_HOST_ARCH=\"arm64\" releng-build-package"
 
 echo "END build"
-
-echo "REOWN \*"
-chown $SUDO_USER:$SUDO_USER -R * # usually the username is also the groupname.
