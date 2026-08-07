8dcbd08667e007a812dfaa814f60c2f7aff2cf46
Oscar Allegro <111370306+SCOS100@users.noreply.github.com>
2026-08-07 18:11:17 +0200

    Update build_droidian.sh

--- build_droidian.sh
+++ build_droidian.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "CREATE dirs"
+cd kernel
 mkdir -p debian/source
 cp arch/arm64/configs/spes_kernel_info.mk debian/kernel-info.mk
 echo "3.0 (native)" > debian/source/format
@@ -17,8 +18,10 @@ chmod +x debian/rules
 echo "CLEAN out"
 rm -f ./out/*.deb ./out/*.build* ./out/*.changes
 
+cd ..
+
 echo "START build"
-CONTAINER_HASH=$(docker run --detach --privileged --cgroupns=host -v ./out:/buildd/out -v ${PWD}:/buildd/sources --security-opt seccomp:unconfined quay.io/droidian/build-essential:next-amd64 sleep infinity)
+CONTAINER_HASH=$(docker run --detach --privileged --cgroupns=host -v ./out:/buildd/out -v ${PWD}/kernel:/buildd/sources --security-opt seccomp:unconfined quay.io/droidian/build-essential:next-amd64 sleep infinity)
 docker exec $CONTAINER_HASH bash -c "apt install linux-packaging-snippets; cd buildd/sources; rm -f debian/control; debian/rules debian/control; RELENG_HOST_ARCH=\"arm64\" releng-build-package"
 
 echo "END build"
